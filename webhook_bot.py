16a795b71bd3961cde32653e4f2649678d768677
nelmy17 <nelson80123@gmail.com>
2025-06-27 14:01:04 +0800

    Update webhook_bot.py

--- webhook_bot.py
+++ webhook_bot.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 BOT_TOKEN = os.environ.get("7308283803:AAHm3CmrIlpGoehyAhX9xgJdAzTn_bZcJcU")
 CHAT_ID = os.environ.get("6748992445D")
-bot = Botbot = Bot(token=os.environ.get("7308283803:AAHm3CmrIlpGoehyAhX9xgJdAzTn_bZcJcU"))
+bot = Bot(token=os.environ.get("7308283803:AAHm3CmrIlpGoehyAhX9xgJdAzTn_bZcJcU"))
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
